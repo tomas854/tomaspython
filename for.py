f10b0811251f3d6450f883fadcf17f47f327050a
tomas854 <to.rubio@duocuc.cl>
2026-04-24 14:55:05 -0400

    ultimos ejercicios

--- for.py
+++ for.py
@@ -16,15 +16,25 @@
 #     print("su tabla de multiplicar: ", num , "x", i, "=", i*num)
 # print(f"{num}x{1}={num*i}")
 
-notas=int(input("ingrese sus notas: "))
-suma=0
-for i in range(notas):
-    n=float(input(f"ingrese la nota {i+1}: "))
-    suma=suma+n
-prom=suma/notas
-print("el promedio es", round,{prom,1} )
+# notas=int(input("ingrese sus notas: "))
+# suma=0
+# for i in range(notas):
+#     n=float(input(f"ingrese la nota {i+1}: "))
+#     suma=suma+n
+# prom=suma/notas
+# print("el promedio es",round{prom,1} )
 
-if prom>=4:
-    print("alumno aprobo")
-else:
-    print("alumno reprobo")    
+# if prom>=4:
+#     print("alumno aprobo")
+# else:
+#     print("alumno reprobo")    
+
+num=int(input("ingrese un numero "))
+total=0
+for i in range(1,num+1):
+    total+=i
+print(f"el resutaldo es: {total}")
+
+
+
+
